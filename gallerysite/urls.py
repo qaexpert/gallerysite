5201186888b8e2b748d643ad0f4594543f26da02
Dmitrii Zhabin <d_zhabin@mail.ru>
2023-04-10 03:58:11 +0300

    Refactoring index.html 02

--- gallerysite/urls.py
+++ gallerysite/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
 from gallery.views import index_page
 
 urlpatterns = [
     path( "admin/", admin.site.urls ),
     path( '', index_page ),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+urlpatterns += static( settings.STATIC_URL, document_root=settings.STATIC_ROOT )
