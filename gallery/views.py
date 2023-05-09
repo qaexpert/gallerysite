d1b5d385e3d32e3bd6522e6ee9290d2b8f7bbe4f
Dmitrii Zhabin <d_zhabin@mail.ru>
2023-05-09 23:45:31 +0300

    Refactoring version 2.0.1-alpha.04

--- gallery/views.py
+++ gallery/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def base_page(request):
+    return render( request, 'gallery/base.html' )
+
 def index_page(request):
     return render( request, 'gallery/index.html' )
